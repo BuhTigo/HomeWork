d33fb225681c7032ab6d7cca67ff91fd1d568ed4
Buh <buh@tig.com>
2014-05-21 17:11:17 +0300

    first commit

---
+++ HomeWork/src/com/buhtigo/lesson1/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.buhtigo.lesson1;
+
+/**
+ * @author ATR
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		// TODO Auto-generated method stub
+
+	}
+
+}
