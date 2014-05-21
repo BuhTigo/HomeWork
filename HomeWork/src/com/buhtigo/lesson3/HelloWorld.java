f1680127aeb0f34313af3df70e6d038d09e11323
Buh <buh@tig.com>
2014-05-21 17:39:48 +0300

    #2

---
+++ HomeWork/src/com/buhtigo/lesson3/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.buhtigo.lesson3;
+
+/**
+ * @author ATR
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello " + args[0] + "!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
